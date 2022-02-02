796395310638d4976d751b68cccdc582c917c850
rpxs <me@rpxs.me>
2022-02-02 01:00:01 -0700

    slatty

--- pages/expo.jsx
+++ pages/expo.jsx
@@ -15,15 +15,21 @@ export default function Home() {
           <motion.h1
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 2.2 }}
+            transition={{ duration: 1.2 }}
             className="text-white mx-14 font-extrabold text-6xl sm:text-6xl md:text-6xl lg:text-7xl xl:text-7xl mt-6"
           >
-            TSA Conference{" "}
-            <span className="text-transparent bg-clip-text bg-gradient-to-tr from-sky-800 to-emerald-500">
-              2022
-            </span>
+            Expo
           </motion.h1>
         </div>
+        <p className="text-white">
+sponsor 1
+        </p>
+        <p className="text-white">
+sponsor 2
+        </p>
+        <p className="text-white">
+sponsor 3
+        </p>
       </div>
     </html>
   );
